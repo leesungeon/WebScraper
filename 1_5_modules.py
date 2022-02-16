0fe9c48358c7ac6ac41a5430f3de069d5df37734
sungeonlee <eonion1995@gmail.com>
2022-02-16 18:46:30 +0900

    Module

---
+++ 1_5_modules.py
@@ -0,0 +1,12 @@
+#import math
+#print(math.ceil(1.2))
+#print(math.fabs(-1.2))
+
+from math import ceil, fsum as sexy
+
+print(ceil(1.2))
+print(sexy([1, 2, 3, 4, 5, 6, 7]))
+
+import calculator as calcal
+
+print(calcal.sum(3, 4))
